837d3f1bbd8bd11bf492f76ed50428d89a00fd01
DaniaAbughoush <ms1dodo@gmail.com>
2021-05-09 00:07:37 +0300

    finish lab08

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const app = express()
 require('dotenv').config()
 
 const WEATHER_API_KEY=process.env.WEATHER_API_KEY
+const MOVIE_API_KEY=process.env.MOVIE_API_KEY
 const PORT=process.env.PORT||1314;
 
 const weather=require('./data/weather.json')
@@ -19,7 +20,7 @@ const superagent = require('superagent');
 
     try {
         console.log(req.query);
-        const weatherBitUrl = `https://api.weatherbit.io/v2.0/forecast/daily?key=${WEATHER_BIT_KEY}&lat=${req.query.lat}&lon=${req.query.lon}`;
+        const weatherBitUrl = `https://api.weatherbit.io/v2.0/forecast/daily?key=${WEATHER_API_KEY}&lat=${req.query.lat}&lon=${req.query.lon}`;
         console.log(weatherBitUrl);
         superagent.get(weatherBitUrl).then(weatherBitData => {
             const arrOfData = weatherBitData.body.data.map(data => new Weather(data));
@@ -37,6 +38,17 @@ const superagent = require('superagent');
 
 })
 
+app.get('/movie', function (req, res) {
+
+  const movieBitUrl = `https://api.themoviedb.org/3/search/movie?api_key=${MOVIE_API_KEY}&query=${req.query.query}&limit=8`;
+
+  superagent.get(movieBitUrl).then(movieBitData => {
+    const movieArray = movieBitData.body.results.map(mov => new Movie(mov));
+    res.send(movieArray);
+  }).catch(console.error);
+
+});
+
 app.get('/', function (req, res) {
   res.send('welcome to backend')
 })
@@ -48,7 +60,15 @@ class Weather{
     
   }
 }
-
+class Movie {
+  constructor(mov) {
+    this.title = mov.title;
+    this.poster = mov.poster_path;
+    this.overview = mov.overview;
+    this.vote_count = mov.vote_count;
+    this.release_date = mov.release_date;
+  }
+}
  
 app.listen(PORT,()=>{
   console.log(`server started at ${PORT}`);
