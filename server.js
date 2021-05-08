2dd04a58c83e88a75ea234404ab49e0fe0e87c64
DaniaAbughoush <ms1dodo@gmail.com>
2021-05-08 16:10:49 +0300

    not working proplem in weatherinfo not array

--- server.js
+++ server.js
@@ -1,19 +1,47 @@
 const express = require('express')
 const app = express()
-const weather=require('./data/weather.json')
-const PORT=process.env.PORT||3030
-const cors = require('cors')
+
 require('dotenv').config()
-app.use(cors())
-app.get('/weather', function (req, res) {
-  const weatherAraay=weather.data.map(value=> new Forcast(value) )
-  res.send(weatherAraay)
-})
-app.get('/', function (req, res) {
-  res.send('hhh')
+
+const WEATHER_API_KEY=process.env.WEATHER_API_KEY
+const PORT=process.env.PORT||1314;
+
+const weather=require('./data/weather.json')
+
+
+const cors = require('cors')
+app.use(cors());
+
+const superagent = require('superagent');
+
+    
+  app.get('/weather', (req, res) => {
+
+    try {
+        console.log(req.query);
+        const weatherBitUrl = `https://api.weatherbit.io/v2.0/forecast/daily?key=${WEATHER_BIT_KEY}&lat=${req.query.lat}&lon=${req.query.lon}`;
+        console.log(weatherBitUrl);
+        superagent.get(weatherBitUrl).then(weatherBitData => {
+            const arrOfData = weatherBitData.body.data.map(data => new Weather(data));
+            res.send(arrOfData);
+
+        });
+    } catch (error) {
+        const arrOfData = weather.data.map(data => new Weather(data));
+        res.send(arrOfData);
+    }
+   
+  
+  // console.log(req.query)
+  // const weatherAraay=weather.data.map(value=> new Forcast(value) )
+
 })
 
-class Forcast{
+app.get('/', function (req, res) {
+  res.send('welcome to backend')
+})
+
+class Weather{
   constructor(weth){
     this.description=weth.weather.description,
     this.date=weth.valid_date
@@ -21,7 +49,7 @@ class Forcast{
   }
 }
 
- console.log('hhh back end')
+ 
 app.listen(PORT,()=>{
   console.log(`server started at ${PORT}`);
 })
